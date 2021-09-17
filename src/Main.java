12391f9511549c4fd91781d10ae5ae997b2b42f0
Ekaterina Miheeva <karamilllka@gmail.com>
2021-09-17 11:43:02 +0600

    lecture2-1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20000;
+        int bonusMiles = ticketPrice / 20;
+        System.out.println("Вам начисленно " + bonusMiles + " бесплатных миль");
+    }
+}
