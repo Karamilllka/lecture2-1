337ed369ee8591065d460c9bae1e9ac459ff1fee
Ekaterina Miheeva <karamilllka@gmail.com>
2021-10-04 17:57:36 +0600

    proba

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 20000;
-        int bonusMiles = ticketPrice / 20;
+        int ticketPrice = 10000;
+        int bonusMiles = ticketPrice / 10;
         System.out.println("Вам начисленно " + bonusMiles + " бесплатных миль");
     }
 }
